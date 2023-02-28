e7798fe21fd560a74a6a5cb26e508c54cfa16c80
Orlando Reynoso <orlandoreynoso1983@gmail.com>
2023-02-28 15:23:13 -0500

    Update

--- js/script.js
+++ js/script.js
@@ -14,3 +14,31 @@ function updateClassActive(element) {
   $('.ml-auto li').removeClass('active');
   $(element).parents('li').addClass('active');
 }
+
+
+$(window).off('scroll');
+
+$(window).on('scroll', function(){
+
+    let scrollTop = $(window).scrollTop() + 15;
+
+    let home = getSectionOffsetTop('hero');
+    let about = getSectionOffsetTop('about');
+    let projects = getSectionOffsetTop('projects');
+    let contacts = getSectionOffsetTop('contacts');
+    
+    if (scrollTop >= home)         setActiveItem('hero');
+    if (scrollTop >= about)        setActiveItem('about');
+    if (scrollTop >= projects)     setActiveItem('projects');
+    if (scrollTop >= (contacts - 700)) setActiveItem('contacts');
+});
+
+function setActiveItem(id){
+    $('#navbarNav li').removeClass('active');
+    $(`a[href="#${id}"]`).parent().addClass('active');
+    
+}
+
+function getSectionOffsetTop(id){
+    return $(`#${id}`).offset().top;
+}
